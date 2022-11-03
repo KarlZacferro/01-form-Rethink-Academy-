7416fb4597f7d5802184a65b04535449cf7ebc5c
Karlisson <karlissonferreira42@gmail.com>
2022-11-03 17:59:10 -0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import { Form } from '@unform/web';
+import * as Yup from 'yup'
 import './App.css';
 
+import Input from './components/Form/Input';
+
+const initialData = {
+}
+
+
 function App() {
+  const formRef = useRef(null);
+
+  
+  async function handleSubmit(data, {reset}){
+    try{
+      //
+      const schema = Yup.object().shape({
+        name: Yup.string().required('o nome é obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('O e-mail é obrigatório'),
+         phoneNumber: Yup.string()
+          .required('apenas numeros') 
+      });
+  
+      await schema.validate(data,{
+          abortEarly: false,
+      })  
+  
+      console.log(data);
+  
+      formRef.current.setErrors({});
+  
+      reset();
+      } catch(err){
+        if(err instanceof Yup.ValidationError){
+          const errorMessage ={};
+  
+          err.inner.forEach(error =>{
+            errorMessage[error.path] = error.message;
+          })
+  
+          formRef.current.setErrors(errorMessage);
+
+          console.log(data);
+        }
+      }
+    }
+ 
+
+  useEffect(()=> {
+    setTimeout(()=>{
+      formRef.current.getData({
+        name: 'Gabriel Gomes',
+        email:'programador@rethink.dev',
+        idade:'25',
+        profissão:'Programador',
+        telefone:'(31)9-9999-9999',
+        dataDePreenchimento:'19/10/2022',
+
+      })
+      
+    },2000);
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Formulario Detran</h1>
+
+      <Form ref={formRef}  initialData={initialData} onSubmit={handleSubmit}>
+        <Input type="name"  name="name" label="Nome" />
+        <Input type="number"  label="Idade" name="idade"/>
+        <Input type="name" label="Profissão" name="profissão"/>
+        <Input type="email" label="E-mail" name="email"/>
+        <Input type="string" label="Telefone" name="telefone"/>
+        <Input type="data" label="Data de Preenchimento" name="data de preenchimento"/>
+        
+      
+        <button type="submit">Enviar</button>
+      </Form>
     </div>
   );
 }
